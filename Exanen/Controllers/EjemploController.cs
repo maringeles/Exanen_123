d5f0ee877e7951468d6d31f101cdd7c3a519ff24
maringeles <angeles.mpa20@gmail.com>
2024-03-24 21:55:41 -0400

    Agregar archivos de proyecto.

---
+++ Exanen/Controllers/EjemploController.cs
@@ -0,0 +1,23 @@
+﻿using Exanen.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exanen.Controllers
+{
+    public class EjemploController : Controller
+    {
+        public IActionResult EjemploObjeto()
+        {
+            Producto prod = new Producto
+            {
+                Id = 1,
+                Nombre = "Queso",
+                Precio = 20.5,
+                Foto = "queso.jpeg",
+                Cantidad = 3
+            };
+                
+            ViewBag.producto= prod;
+            return View();
+        }
+    }
+}
---
+++ Exanen/Models/Producto.cs
@@ -0,0 +1,11 @@
+﻿namespace Exanen.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public Double Precio { get; set; }
+        public string Foto { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
